cd57b2e4f6dbcea906ceecf6ef0241f89b45704c
huangyp <huangyp@haoctech.com>
2019-03-08 16:24:26 +0800

    test

---
+++ src/main/java/com/example/harrysprbot/HarrysprbotApplication.java
@@ -0,0 +1,14 @@
+package com.example.harrysprbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarrysprbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarrysprbotApplication.class, args);
+        System.out.println("启动成功！！！！");
+    }
+
+}
